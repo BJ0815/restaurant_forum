7859906e5e83b4e7846a07dc72225bf453c480c6
BJ0815 <jtt11220@gmail.com>
2017-12-12 14:42:31 +0800

    setup restaurant resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  #Devise 自動建立認證使用者功能
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  #前台路由設定的餐廳資源(只限定開放閱覽功能)
+  resources :restaurants, only: [:index, :show]
   root "restaurants#index"
 
+  #後台路由設定的餐廳資源(resources會開放7種action)
   namespace :admin do
     resources :restaurants
     resources :categories
