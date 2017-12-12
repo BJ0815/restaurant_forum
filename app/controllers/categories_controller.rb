d33314ba9e7389ac8c28114c12c4f573d9859436
BJ0815 <jtt11220@gmail.com>
2017-12-12 17:16:11 +0800

    user on index page can select restaurants by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,10 @@
 class CategoriesController < ApplicationController
+
+  def show
+    @categories = Category.all
+    @category = Category.find(params[:id])
+    #運用關聯設定
+    @restaurants = @category.restaurants.page(params[:page]).per(9)
+  end
+
 end
