913d4ea8e6257df935613d3d190e908c72841185
BJ0815 <jtt11220@gmail.com>
2017-12-16 13:56:56 +0800

    setup comment resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   #前台路由設定的餐廳資源(只限定開放閱覽功能)
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [:index, :show] do
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :categories, only: [:show]
   root "restaurants#index"
 
