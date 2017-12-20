c01a5b061813aa3bb0df07855c8f92a3c5b5d01d
BJ0815 <jtt11220@gmail.com>
2017-12-20 13:34:50 +0800

    user controller add show and edit action and created views page of both

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    unless @user.name? && @user.intro?
+      redirect_to edit_user_path(@user)
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
 end
