f1cb4c2d11a50f241b6dd6c9d5257723b74409fa
BJ0815 <jtt11220@gmail.com>
2017-12-20 14:21:48 +0800

    setup edit and update action, setup the edit views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,35 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
+    #如果使用者資料未齊全，直接跳往edit頁面進行設定
     unless @user.name? && @user.intro?
       redirect_to edit_user_path(@user)
     end
   end
 
-  def edit
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:notice] = "User's profile is successfully updated"
+      redirect_to user_path(@user)
+    else
+      flash.now[:alert] = "User's profile is failed updated,plaese check it again!"
+      render :edit
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :intro)
+  end
+
+  def set_user
     @user = User.find(params[:id])
   end
+
+
 
 end
