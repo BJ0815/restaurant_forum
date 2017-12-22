aa3067d46697ee6e312311314256e5ca461dd033
BJ0815 <jtt11220@gmail.com>
2017-12-22 14:17:04 +0800

    build the relation between user and restaurant, setup the user's show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  #關聯設定
+  #關聯設定(與comments一對多關係及與restaurants多對多關係)
   has_many :comments
+  has_many :restaurants, through: :comments
 
   #將PhotoUploader掛載上去，mount_uploader是carrierwave提供得掛載方法
   mount_uploader :avatar, PhotoUploader
