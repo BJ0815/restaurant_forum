1058f8db5023fad3cd863ab619814ff21a6147fd
BJ0815 <jtt11220@gmail.com>
2018-01-08 12:45:24 +0800

    create action for friend record

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,13 @@
 class FriendshipsController < ApplicationController
+
+  def create
+    @friend = current_user.friendships.new(friend_id: params[:friend_id])
+    if @friend.save
+      flash[:notice] = "成功加入好友"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @friend.errors.full_message?.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
+  end
 end
