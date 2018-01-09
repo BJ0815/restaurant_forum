56059d79b1482ee082d8407d2d10f104c74f1c3e
BJ0815 <jtt11220@gmail.com>
2018-01-09 22:31:26 +0800

    add after_create callback action in friendship.rb

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,16 @@
 class Friendship < ApplicationRecord
+  after_create :inverse_friend
+
+  validates :user_id , uniqueness: { scope: :friend_id }
+
   belongs_to :user
   belongs_to :friend, class_name: "User"
+
+  private
+
+  def inverse_friend
+    Friendship.create(friend_id: user, user_id: friend, status: "applying")
+  end
+
+
 end
